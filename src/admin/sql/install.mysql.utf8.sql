f6104e81fafe3efa3770d35d9a6f2670c403de47
sushan <sushan_g@tekditechnologies.com>
2016-12-09 16:01:59 +0530

    correction in table structure

--- src/admin/sql/install.mysql.utf8.sql
+++ src/admin/sql/install.mysql.utf8.sql
@@ -8,17 +8,16 @@ CREATE TABLE `#__importer_batches` (
   `created_date` datetime NOT NULL,
   `updated_date` datetime NOT NULL,
   `import_user` int(10) NOT NULL,
-  `params` datetime NOT NULL,
+  `params` LONGTEXT NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
-
 
 CREATE TABLE IF NOT EXISTS `#__importer_items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `batch_id` int(11) NOT NULL,
   `validated` tinyint(1) NOT NULL,
   `imported` tinyint(1) NOT NULL,
-  `invalid_columns` TEXT,
+  `invalid_columns` LONGTEXT,
   `data` LONGTEXT NOT NULL,
   `content_id` varchar(250),
   `import_error` varchar(250),
