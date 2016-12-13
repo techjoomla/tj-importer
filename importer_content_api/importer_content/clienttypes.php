d8e8ad69536d294601f99d24c84b70c00a8db5eb
sushan <sushan_g@tekditechnologies.com>
2016-12-13 12:04:45 +0530

    Task #93793 - get categories/types

--- importer_content_api/importer_content/clienttypes.php
+++ importer_content_api/importer_content/clienttypes.php
@@ -10,34 +10,6 @@
 defined('_JEXEC') or die;
 jimport('joomla.plugin.plugin');
 
-/**
- * Clienttypes Resource for Importer_zoo Plugin.
- *
- * @since  2.5
- */
-class Importer_ContentApiResourceClienttypes extends ApiResource
-{
-	/**
-	 * GET function to fetch different types/categories in zoo
-	 *
-	 * @return  JSON  types details
-	 * 
-	 * @since  3.0
-	 **/
-	public function get()
-	{
-		<?php
-/**
- * @package     Joomla.Plugin
- * @subpackage  Importer_zoo
- *
- * @copyright   Copyright (C) 2016 - 2017 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-defined('_JEXEC') or die;
-jimport('joomla.plugin.plugin');
-
 JLoader::import('components.com_categories.models.categories', JPATH_ADMINISTRATOR);
 
 /**
@@ -70,22 +42,6 @@ class Importer_ContentApiResourceClienttypes extends ApiResource
 
 		$this->plugin->setResponse($returnCat);
 		die("In get function of importer_content");
-	}
-
-	/**
-	 * POST function unnecessary
-	 *
-	 * @return  STRING error message
-	 * 
-	 * @since  3.0
-	 **/
-	public function post()
-	{
-		// $this->plugin->setResponse("POST method is not supporter, try GET method");
-		die("in post funtion");
-	}
-}
-
 	}
 
 	/**
